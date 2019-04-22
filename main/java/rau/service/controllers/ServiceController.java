61cbb01ae5a05decc3fd2425ffbe8864c68a4a4d
Armen080598 <aaraqelyan.araqelyan@gmail.com>
2019-04-23 02:21:28 +0400

    sending dataa to front

--- main/java/rau/service/controllers/ServiceController.java
+++ main/java/rau/service/controllers/ServiceController.java
@@ -1,12 +1,11 @@
 package rau.service.controllers;
 
 import com.google.gson.Gson;
+import com.sun.org.apache.xpath.internal.operations.Mult;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.xml.sax.SAXException;
 import rau.service.service.XMLService;
@@ -19,9 +18,12 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
+@CrossOrigin("*")
 public class ServiceController {
 
     private XMLService xmlService;
+
+    public static MultipartFile lastUploadedFile;
 
 
     public ServiceController(XMLService xmlService){
@@ -35,8 +37,16 @@ public class ServiceController {
 //    }
 
     @PostMapping(value = "/xml", produces = MediaType.APPLICATION_XML_VALUE)
-    public ResponseEntity processXMLFile(@RequestParam MultipartFile file) throws XMLStreamException, IOException, ParserConfigurationException, SAXException {
+    public ResponseEntity processXMLFile(@RequestParam("file") MultipartFile file) throws XMLStreamException, IOException, ParserConfigurationException, SAXException {
+        lastUploadedFile = file;
         List xmlProcessingResult = this.xmlService.processAlternative(file);
+        return new ResponseEntity<>(new Gson().toJson(xmlProcessingResult), HttpStatus.OK);
+    }
+
+    @CrossOrigin(origins = "*")
+    @GetMapping(value = "/xml/latest", produces = MediaType.APPLICATION_XML_VALUE)
+    public ResponseEntity processXMLFile() throws XMLStreamException, IOException, ParserConfigurationException, SAXException {
+        List xmlProcessingResult = this.xmlService.processAlternative(lastUploadedFile);
         return new ResponseEntity<>(new Gson().toJson(xmlProcessingResult), HttpStatus.OK);
     }
 
--- main/java/rau/service/service/XMLService.java
+++ main/java/rau/service/service/XMLService.java
@@ -102,7 +102,7 @@ public class XMLService {
         }
     }
 
-    public static File convert(MultipartFile file) throws IOException {
+    public File convert(MultipartFile file) throws IOException {
         File convFile = new File(file.getOriginalFilename());
         convFile.createNewFile();
         FileOutputStream fos = new FileOutputStream(convFile);
